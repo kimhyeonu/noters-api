e2bc360b8eeee1624bdee86a29ac8e3128594d84
kimhyeonu <polaris@byeolmuri.com>
2021-06-10 01:30:14 +0900

    사용자(User) 스키마 및 모델 구현

--- src/schema.js
+++ src/schema.js
@@ -6,9 +6,17 @@ module.exports = gql`
   type Note {
     id: ID!
     content: String!
-    author: String!
+    author: User!
     createdAt: DateTime!
     updatedAt: DateTime!
+  }
+
+  type User {
+    id: ID!
+    username: String!
+    email: String!
+    avatar: String
+    notes: [Note!]!
   }
 
   type Query {
@@ -17,8 +25,11 @@ module.exports = gql`
   }
 
   type Mutation {
-    createNote(content: String!): Note
+    createNote(content: String!): Note!
     updateNote(id: ID!, content: String!): Note!
     deleteNote(id: ID!): Boolean!
+
+    signUp(username: String!, email: String!, password: String!): String!
+    signIn(username: String, email: String, password: String!): String!
   }
 `;
