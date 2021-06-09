1a63846a5a99998682ff2dfdb3769111463bd895
kimhyeonu <polaris@byeolmuri.com>
2021-06-09 12:13:36 +0900

    In-memory GraphQL query, mutation 테스트

--- src/index.js
+++ src/index.js
@@ -3,10 +3,28 @@ const { ApolloServer, gql } = require('apollo-server-express');
 
 const port = process.env.PORT || 4000;
 
+let notes = [
+  { id: '1', content: 'This is a note.', author: 'Adam Scott' },
+  { id: '2', content: 'This is another note.', author: 'Harlow Everly' },
+  { id: '3', content: 'Oh hey look, another note!', author: 'Riley Harrison' },
+];
+
 // GraphQL 스키마 언어로 스키마를 구성한다.
 const typeDefs = gql`
+  type Note {
+    id: ID!
+    content: String!
+    author: String!
+  }
+
   type Query {
-    hello: String
+    hello: String!
+    notes: [Note!]!
+    note(id: ID!): Note!
+  }
+
+  type Mutation {
+    newNote(content: String!): Note!
   }
 `;
 
@@ -14,6 +32,22 @@ const typeDefs = gql`
 const resolvers = {
   Query: {
     hello: () => 'Hello, World!',
+    notes: () => notes,
+    note: (parent, args) => {
+      return notes.find((note) => note.id === args.id);
+    },
+  },
+
+  Mutation: {
+    newNote: (parent, args) => {
+      let noteValue = {
+        id: String(notes.length + 1),
+        content: args.content,
+        author: 'Adam Scott',
+      };
+      notes.push(noteValue);
+      return noteValue;
+    },
   },
 };
 
