85d2d6220e7063e0f5b3adc247324b7dda5abb76
kimhyeonu <polaris@byeolmuri.com>
2021-06-09 11:57:07 +0900

    Apollo GraphQL 애플리케이션 구현

--- src/index.js
+++ src/index.js
@@ -1,8 +1,32 @@
 const express = require('express');
+const { ApolloServer, gql } = require('apollo-server-express');
 
-const app = express();
 const port = process.env.PORT || 4000;
 
-app.get('/', (req, res) => res.send('I am a JavaScript Master!!!'));
+// GraphQL 스키마 언어로 스키마를 구성한다.
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
 
-app.listen(port, () => console.log(`Server running at http://localhost:${port}`));
+// 스키마 필드를 위한 리졸버 함수를 제공한다.
+const resolvers = {
+  Query: {
+    hello: () => 'Hello, World!',
+  },
+};
+
+// 익스프레스 애플리케이션을 생성한다.
+const app = express();
+// 아폴로 서버를 설정한다.
+const server = new ApolloServer({ typeDefs, resolvers });
+
+// 아폴로 GraphQL 미들웨어를 적용하고 경로를 '/api'로 설정한다.
+server.applyMiddleware({ app, path: '/api' });
+
+app.listen({ port }, () =>
+  console.log(
+    `GraphQL Server running at http://localhost:${port}${server.graphqlPath}`
+  )
+);
